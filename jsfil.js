de2448b86d1a57c6d843ff4104408f266b897bde
Athulya Thomas <athulyathomas2398@gmail.com>
2024-10-19 14:50:49 +0530

    calculator using js

---
+++ jsfil.js
@@ -0,0 +1,21 @@
+const displayContent=(content)=>{
+    display.value+=content
+    
+}
+showResult=()=>{
+    try{
+        display.value=eval(display.value)
+    }
+    catch{
+        display.value=" "
+        display.placeholder="Invalid Expression"
+    }
+}
+clearAll=()=>{
+    display.value=" "
+    display.placeholder=0
+}
+Remove=()=>{
+    display.value=display.value.slice(0,-1)
+    
+}
